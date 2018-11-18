ede55240cf1ffaeaf5614c32a8d6777273b8b75e
Vladyslav Melnyk <v.v.melnyk1994@gmail.com>
2018-11-18 23:05:07 +0200

    init commit

---
+++ src/main/kotlin/com.coinselection/UnspentOutput.kt
@@ -0,0 +1,17 @@
+package com.coinselection
+
+import java.math.BigDecimal
+
+data class UnspentOutput(
+        val txid: String? = null,
+        val vout: Int? = null,
+        val label: String? = null,
+        val address: String? = null,
+        val scriptPubKey: String? = null,
+        val amount: BigDecimal,
+        val confirmations: Int? = null,
+        val redeemScript: String? = null,
+        val spendable: Boolean? = null,
+        val solvable: Boolean? = null,
+        val safe: Boolean? = null
+)
