f06685ea6ecaa83ce3005abb35b969ad477bf373
Vladyslav Melnyk <v.v.melnyk1994@gmail.com>
2018-12-31 02:14:00 +0200

    change function calls

--- src/main/kotlin/com.coinselection/BtcCoinSelectionProvider.kt
+++ src/main/kotlin/com.coinselection/BtcCoinSelectionProvider.kt
@@ -22,7 +22,7 @@ class BtcCoinSelectionProvider(
         val selectedUtxoList = select(utxoList, targetValue, costCalculator, cumulativeHolder)
                 ?: return null
         val improvedUtxoList = improve(getRemainingUtxoList(selectedUtxoList = selectedUtxoList, originalUtxoList = utxoList), targetValue = targetValue, costCalculator = costCalculator, cumulativeHolder = cumulativeHolder)
-        return CoinSelectionResult(selectedUtxos = mergeUtxoLists(selectedUtxoList = selectedUtxoList, improvedUtxoList = improvedUtxoList), totalFee = cumulativeHolder.accumulatedFee.get())
+        return CoinSelectionResult(selectedUtxos = mergeUtxoLists(selectedUtxoList = selectedUtxoList, improvedUtxoList = improvedUtxoList), totalFee = cumulativeHolder.getFee())
     }
 
     private fun select(utxoList: List<UnspentOutput>, targetValue: BigDecimal, costCalculator: CostCalculator, cumulativeHolder: CumulativeHolder): List<UnspentOutput>? {
@@ -63,7 +63,7 @@ class BtcCoinSelectionProvider(
                 .asSequence()
                 .take(maxNumberOfInputs)
                 .takeWhile { cumulativeHolder.getSum() < maxTargetValue + cumulativeHolder.getFee() }
-                .onEach { delta.getAndSet((cumulativeHolder.accumulatedSum.get() - (optimalTargetValue + cumulativeHolder.getFee())).abs()) }
+                .onEach { delta.getAndSet((cumulativeHolder.getSum() - (optimalTargetValue + cumulativeHolder.getFee())).abs()) }
                 .takeWhile { (cumulativeHolder.getSum() + it.amount - (optimalTargetValue + cumulativeHolder.getFee() + costCalculator.getCostPerInput())).abs() < delta.get() }
                 .onEach { appendCumulativeHolder(cumulativeHolder = cumulativeHolder, costCalculator = costCalculator, sum = it.amount) }
                 .toList()
